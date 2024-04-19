b44a2d72b31a5c418eb0bdc2be6ed735bf4b64d8
Sam Lau <samlau95@gmail.com>
2024-04-19 11:12:54 -0700

    add extra parse test for groupby_aggs

--- pandas_tutor/tests/parse_golden/groupby_aggs.py
+++ pandas_tutor/tests/parse_golden/groupby_aggs.py
@@ -8,6 +8,13 @@
  .var()
 )
 
+# Note that this is an agg but we have to handle it at runtime, not parse time
+(dogs
+ .groupby('grooming')
+ ['weight']
+ .var()
+)
+
 # should not be parsed into an AggCall
 (dogs
  .groupby('grooming')
