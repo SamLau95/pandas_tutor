7e07dcf2b16603c409eb038fb8c1c15ea7715ca0
ylesia-wu <130006120+ylesia-wu@users.noreply.github.com>
2024-04-22 16:57:39 -0700

    added golden files and comment for cross out

--- pandas_tutor/marks/marks.py
+++ pandas_tutor/marks/marks.py
@@ -160,10 +160,10 @@ def mark_for_groupby_filter(step: GroupByFilterCall, before: EvalResult, after: 
 
     # get the cross out from lhs
     before_label = set(before_val.index)
+    # find indices of dropped rows when dropna=False
     after_label = set(after.val[after.val.notna().any(axis=1)].index)
     rows_to_drop = before_label.difference(after_label)
     cross = make_drops(rows_to_drop, "row")
-    breakpoint()
     return [*arrow, *cross]
 
 # df.get(['Name'])
