ed32e277b14d853f8908008ef532efa6c58828f1
ylesia-wu <130006120+ylesia-wu@users.noreply.github.com>
2024-04-22 18:15:29 -0700

    edit comment for cross out mark and remove breakpoint()

--- pandas_tutor/marks/marks.py
+++ pandas_tutor/marks/marks.py
@@ -160,7 +160,7 @@ def mark_for_groupby_filter(step: GroupByFilterCall, before: EvalResult, after: 
 
     # get the cross out from lhs
     before_label = set(before_val.index)
-    # find indices of dropped rows when dropna=False
+    # find indices of not-dropped rows and take care of dropna=False
     after_label = set(after.val[after.val.notna().any(axis=1)].index)
     rows_to_drop = before_label.difference(after_label)
     cross = make_drops(rows_to_drop, "row")
@@ -237,7 +237,7 @@ def mark_for_drop(
     row_labels = args.get("row_labels", [])
     if not util.is_list_like(row_labels):
         row_labels = [row_labels]
-    breakpoint()
+
     # cross out dropped rows or columns
     return [
         *make_drops(col_labels, "column"),
